867c701e4ee5d5b2cab002bb927540586308686d
jeremiastomas72@gmail.com <90426798+JeremiasTO@users.noreply.github.com>
2021-11-21 04:06:04 -0500

    Add project files.

---
+++ DataDemo20/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using DataDemo20.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataDemo20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult SignIn()
+        {
+            ViewBag.Message = "Please Sign Here.";
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignIn(EmployeeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ DataDemo20/Models/EmployeeModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DataDemo20.Models
+{
+    public class EmployeeModel
+    {
+        [Display(Name = "Employee ID")]
+        [Range(100, 999, ErrorMessage = "You need to enter a valid EmployeeId")]
+        public int EmployeeId { get; set; }
+
+
+        [Display(Name = "FirstName")]
+        [Required(ErrorMessage = "You need to give us your first name.")]
+        public string FirstName { get; set; }
+
+
+        [Display(Name = "LastName")]
+        [Required(ErrorMessage = "You need to give us your last name.")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "The Email and Confirm Email must match")]
+        public string EmailAddress { get; set; }
+
+
+        [Display(Name = "ConfirmEmail")]
+        [Compare("ConfirmEmail", ErrorMessage = "The Email and Confirm Email must match")]
+        public string ConfirmEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please inser your password.")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "You need to provide a long enough password.")]
+        public string Password { get; set; }
+
+        [Display(Name = "Password")]
+        [Compare("Password", ErrorMessage = " The Password and Confirm Password do not match")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        
+        [Display(Name = "SIN")]
+        [Required(ErrorMessage = "Please provide Valid SIN.")]
+        [StringLength(9, MinimumLength = 8, ErrorMessage = "You must provide a valid SIN.")]
+
+        public string SIN { get; set; }
+
+       
+
+       
+
+
+
+
+
+
+
+    }
+}
